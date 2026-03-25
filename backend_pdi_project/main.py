3f62375aa163c1eb9fb989d8244d5d59e8f1e991
Samuel Nogueira <samucans2004@gmail.com>
2026-03-25 20:34:00 -0300

    fisrt

---
+++ backend_pdi_project/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, UploadFile, File, Form
+import cv2
+import numpy as np
+from fastapi.responses import StreamingResponse
+import io
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://localhost:5173"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/teste_de_api")
+async def teste_de_api(file: UploadFile = File(...), valor_sub: int = Form(...)):
+    conteudo = await file.read()
+
+    # bytes → numpy array → imagem
+    np_img = np.frombuffer(conteudo, np.uint8)
+    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+
+    # subtração
+    resultado = cv2.subtract(img, valor_sub)
+
+    # imagem → bytes
+    _, buffer = cv2.imencode(".png", resultado)
+
+    return StreamingResponse(io.BytesIO(buffer.tobytes()), media_type="image/png")
---
+++ pdi_project/src/App.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [resultado, setResultado] = useState<string | null>(null);
+  const [imagem, setImagem] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+  const [valor, setValor] = useState<number>(50);
+
+  const enviarImagem = async () => {
+    if (!imagem) {
+      alert("Selecione uma imagem");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", imagem);
+    formData.append("valor_sub", String(valor));
+
+    const res = await fetch("http://127.0.0.1:8000/teste_de_api", {
+      method: "POST",
+      body: formData,
+    });
+
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+
+    setResultado(url);
+  };
+
+  return (
+    <section id="center">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => {
+          if (e.target.files && e.target.files[0]) {
+            const file = e.target.files[0];
+            setImagem(file);
+            setPreview(URL.createObjectURL(file)); // 👈 preview original
+          }
+        }}
+      />
+
+      <input
+        type="number"
+        value={valor}
+        onChange={(e) => setValor(Number(e.target.value))}
+      />
+
+      <button onClick={enviarImagem}>Enviar</button>
+
+      <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+        {preview && (
+          <div>
+            <h3>Original</h3>
+            <img src={preview} alt="original" width={300} />
+          </div>
+        )}
+
+        {resultado && (
+          <div>
+            <h3>Resultado</h3>
+            <img src={resultado} alt="resultado" width={300} />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default App;
